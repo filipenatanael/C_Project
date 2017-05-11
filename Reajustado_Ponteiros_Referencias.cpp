ddd6862e0e69d5916d723f09c0aa3b298b6d041b
Filipe Natanael <filipenatanael@live.com>
2017-05-11 09:51:14 -0300

    Using pointers and references
    Using pointers and references

---
+++ Reajustado_Ponteiros_Referencias.cpp
@@ -0,0 +1,31 @@
+/* Fazer uma função que recebe como argumento o preço de um produto e o percentual de reajuste de produto.
+A função deve retornar o novo preço */
+
+/* Agora utilizando ponteiros e referências */
+
+#include<stdio.h>
+#include<stdlib.h>
+//Função para fazer o calculo do preço reajustado
+
+void NOVO_PRECO(float *Preco,float *Percentual){
+
+	*Preco = *Preco +(*Preco * *Percentual)/100;
+	
+	
+}
+
+/* Utilizando referência deve ser impresso apenas o preço reajustado, utilizando menos variáveis e assim
+utilizando ponteiros para dar referência do endereço apontado */
+
+int main(){
+	
+	float Preco,Percentual;
+	printf("Digite um preco para o produto: ");
+	scanf("%f",&Preco);
+	printf("Digite um percentual para reajuste do preco do produto: ");
+	scanf("%f",&Percentual);
+	NOVO_PRECO(&Preco,&Percentual);
+	printf("O preco reajustado e: %.2f",Preco);
+	
+}
+
