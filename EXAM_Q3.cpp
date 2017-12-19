fa5105ebd0060eadcac842ca65eb3fea18c2c65c
filipenatanael <filipenatanael@live.com>
2017-12-19 20:08:28 -0200

    Remove Space

--- EXAM_Q3.cpp
+++ EXAM_Q3.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(){
-	
+
 	int matriz[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
 	int vetor[3];
 	int x=0;
@@ -10,19 +10,19 @@ int main(){
 	int total =0;
 	int PDMaisVendeu = 0;
 	int LJVendeuMais = 0;
-	
+
 	//Zerando vetor;
 	for(int z=0; z<3; z++){
-	vetor[z]= 0;	
+	vetor[z]= 0;
 	}
-	
+
 	for( x=0; x<3; x++){
 		for(y=0; y<3; y++){
         vetor[x]= vetor[x] + matriz[y][x];
-        
+
 		}
 	}
-	
+
 	for(int z=0; z<3; z++){
 	printf("\nLoja %d vendeu: %d",z+1,vetor[z]);
 	total = total + vetor[z];
@@ -32,16 +32,12 @@ int main(){
 		 LJVendeuMais = LJVendeuMais +1;
 		 //+1 para controlar o indice do vetor;
 		 }
-		
+
 	}
-	
+
 	printf("\n\nTotal Vendido: %d",total);
 	printf("\n\nProduto Mais Vendido: %d",PDMaisVendeu);
 	printf("\n\nLoja que mais vendeu: %d",LJVendeuMais);
-	
-
-	
-	
 	
 return 0;
 }
